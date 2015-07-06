b57d70d3740f9a7d1201bcb3d608769d4d0a7dca
Kim Dang & Toby Alden <student@epicodus.com>
2015-07-06 10:46:54 -0700

    Added front-end log in / log out.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   get '/' => 'home#index'
   resources :users
+
+  get '/log-in' => 'sessions#new'
+  post '/log-in' => 'sessions#create'
+  delete '/log-out' => 'sessions#destroy', as: :log_out
+
 end
